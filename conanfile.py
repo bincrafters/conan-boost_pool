55727905f24cfc7dcd672cf0029cab334f7c269a
Rene Rivera <grafikrobot@gmail.com>
2018-12-17 14:02:41 -0600

    Initial 1.69.0 packages. [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,12 +4,18 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.68.0@bincrafters/testing")
+base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostPoolConan(base.BoostBaseConan):
     name = "boost_pool"
     url = "https://github.com/bincrafters/conan-boost_pool"
     lib_short_names = ["pool"]
     header_only_libs = ["pool"]
-    cycle_group = "boost_cycle_group_c"
-    b2_requires = ["boost_cycle_group_c"]
+    b2_requires = [
+        "boost_assert",
+        "boost_config",
+        "boost_integer",
+        "boost_throw_exception",
+        "boost_type_traits",
+        "boost_winapi"
+    ]
